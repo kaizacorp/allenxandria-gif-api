bb7dc77b45fb25e0c8676b1c5f56ca50584016c3
Kai <kaicharlton@gmail.com>
2021-11-22 02:17:07 -0600

    refactor to use countDocuments, not  getting all

--- routes/Random.js
+++ routes/Random.js
@@ -2,20 +2,21 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Gif = require("../models/Gif");
-require('dotenv').config();
+require("dotenv").config();
 
 // Get
 router.get("/", async (req, res) => {
   // Database
 
- mongoose.connect(
+  mongoose.connect(
     `mongodb+srv://kaiza:${process.env.PASSWORD}@${process.env.DB_URL}?retryWrites=true&w=majority`
   );
   const db = mongoose.connection;
   //console.log("Connected to MongoDB database...");
-  const all = await Gif.find();
-  random = Math.floor(Math.random() * all.length);
-  res.send(all[random]);
+  const count = await Gif.countDocuments();
+  const random = Math.floor(Math.random() * count);
+  const gif = await Gif.findOne().skip(random);
+  res.send(gif);
 });
 
 module.exports = router;
