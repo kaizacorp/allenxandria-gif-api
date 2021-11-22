303977c744658f7b9cb5a069f38318fb0b53a184
Kai <kaicharlton@gmail.com>
2021-11-22 03:01:13 -0600

    search using params, strict matching only

--- routes/Search.js
+++ routes/Search.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const Gif = require("../models/Gif");
+require("dotenv").config();
 
-// Get all routes
-router.get("/", (req, res) => {
-  res.send("search: get all routes");
+// Get
+router.get("/", async (req, res) => {
+  // Database
+
+  mongoose.connect(
+    `mongodb+srv://kaiza:${process.env.PASSWORD}@${process.env.DB_URL}?retryWrites=true&w=majority`
+  );
+  const db = mongoose.connection;
+  //console.log("Connected to MongoDB database...");
+  const q = await Gif.find({ tags: { $in: req.query.tags } });
+  res.send(q[0]);
 });
 
 module.exports = router;
