e39f021f8a08913b4a22d168d2c070f016b8febe
Kai <kaicharlton@gmail.com>
2021-11-22 02:13:21 -0600

    schema for tags to be String, not Array[String]

--- models/Gif.js
+++ models/Gif.js
@@ -2,11 +2,7 @@ const mongoose = require("mongoose");
 
 const gifSchema = new mongoose.Schema({
   url: String,
-  tags: [
-    {
-      type: String,
-    },
-  ],
+  tags: String,
 });
 
 module.exports = mongoose.model("gif", gifSchema);
