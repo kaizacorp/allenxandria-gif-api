599ea54f42df56724ab6454f3c908699978141ba
Kai <kaicharlton@gmail.com>
2023-01-15 21:03:15 -0600

    support ranom=true on /all route

--- routes/All.js
+++ routes/All.js
@@ -4,15 +4,26 @@ const mongoose = require("mongoose");
 const Gif = require("../models/Gif");
 require("dotenv").config();
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
 // Responds with all Allen gifs
 // defaults to oldest first
-// if recent=true gives newest first
+// if recent=true, gives newest first (overrides random)
+// if random=true, gives all in random order
 router.get("/", async (req, res) => {
   await connectToMongo();
   const db = mongoose.connection;
   const all = await Gif.find();
   if (req.query.recent && req.query.recent === "true") {
     all.reverse();
+    res.send(all);
+  } else if (req.query.random && req.query.random === "true") {
+    shuffle(all);
     res.send(all);
   } else {
     res.send(all);
